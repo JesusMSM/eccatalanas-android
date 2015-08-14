6e2dbc2a5e59b5a814e2c609ec430f736922cc95
JesusMSM <jesusmanuel93@hotmail.com>
2015-08-14 18:00:12 +0200

    Pestañas abajo y graficos empezados

--- app/src/main/java/es/elconfidencial/eleccionesec/fragments/ChartTab.java
+++ app/src/main/java/es/elconfidencial/eleccionesec/fragments/ChartTab.java
@@ -3,24 +3,130 @@ package es.elconfidencial.eleccionesec.fragments;
 /**
  * Created by MOONFISH on 14/07/2015.
  */
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import es.elconfidencial.eleccionesec.R;
-
+import es.elconfidencial.eleccionesec.activities.HomeActivity;
+import es.elconfidencial.eleccionesec.chart.BarChartItem;
+import es.elconfidencial.eleccionesec.chart.ChartItem;
 
 
 public class ChartTab extends Fragment {
 
+    BarChart barChart;
+    ListView lv;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.tab_chart,container,false);
+        View v = inflater.inflate(R.layout.tab_chart, container, false);
+
+        lv = (ListView) v.findViewById(R.id.listView1);
+        ArrayList<ChartItem> list = new ArrayList<ChartItem>();
+        list.add(new BarChartItem(generateDataBar(3), HomeActivity.context));
+
+        ChartDataAdapter cda = new ChartDataAdapter(HomeActivity.context, list);
+        lv.setAdapter(cda);
+       /* barChart = (BarChart) v.findViewById(R.id.barChart1);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+
+        BarDataSet dataset = new BarDataSet(entries, "Leyenda");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("PP");
+        labels.add("PSOE");
+        labels.add("Podemos");
+        labels.add("Ciudadanos");
+
+
+        BarData data = new BarData(labels, dataset);
+        barChart.setData(data);
+        barChart.setDescription("Ejemplo");
+        barChart.animateY(5000);
+
+
+
+        //setContentView(chart);*/
+
         return v;
+    }
+    private BarData generateDataBar(int cnt) {
+
+        ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+
+        for (int i = 0; i < 12; i++) {
+            entries.add(new BarEntry((int) (Math.random() * 70) + 30, i));
+        }
+
+        BarDataSet d = new BarDataSet(entries, "New DataSet " + cnt);
+        d.setBarSpacePercent(20f);
+        d.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        d.setHighLightAlpha(255);
+
+        BarData cd = new BarData(getMonths(), d);
+        return cd;
+    }
+    private ArrayList<String> getMonths() {
+
+        ArrayList<String> m = new ArrayList<String>();
+        m.add("Jan");
+        m.add("Feb");
+        m.add("Mar");
+        m.add("Apr");
+        m.add("May");
+        m.add("Jun");
+        m.add("Jul");
+        m.add("Aug");
+        m.add("Sep");
+        m.add("Okt");
+        m.add("Nov");
+        m.add("Dec");
+
+        return m;
+    }
+    /** adapter that supports 3 different item types */
+    private class ChartDataAdapter extends ArrayAdapter<ChartItem> {
+
+        public ChartDataAdapter(Context context, List<ChartItem> objects) {
+            super(context, 0, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return getItem(position).getView(position, convertView, getContext());
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            // return the views type
+            return getItem(position).getItemType();
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 3; // we have 3 different item-types
+        }
     }
 }
